a227803d1c3f71b3504156b524cd2635f89fdd93
Ramindu Deshapriya <rasade88@gmail.com>
2021-02-26 12:08:43 -0500

    Added gradle and initialized example class

---
+++ src/main/java/com/ramdesh/parquet/example/ParquetExample.java
@@ -0,0 +1,14 @@
+package com.ramdesh.parquet.example;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.parquet.avro.AvroParquetWriter;
+import org.apache.parquet.hadoop.metadata.CompressionCodecName;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ParquetExample {
+}
